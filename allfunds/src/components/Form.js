91e82717916015c5ba9864f039c9e30791d5206a
Patricia Defez <ptddds@gmail.com>
2019-10-14 21:45:15 +0200

    onChange

--- allfunds/src/components/Form.js
+++ allfunds/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 const NUMBER_PATTERN = /^\d*$/;
 
@@ -11,29 +11,35 @@ const validations = {
 
 }
  const Form = () => {
+     
+    const [transportation, setTransportation] = useState("");
+    const [meal, setMeal] = useState(0);
+    const [parking, setParking] = useState("");
+
+
      return (
         <div className="row mx-auto ">
             <div className="col-xs-12 col-sm-10 mx-auto mt-5">
                 <div className = "input-group  col-sm-6 mb-3">
-                    <label for="transportation" class="col-sm-4">Desplazamientos</label>       
-                    <input type="text" readonly class="form-control" name="transportation" />
+                    <label for="transportation" className="col-sm-4">Desplazamientos</label>       
+                    <input type="text"  className="form-control" name="transportation" onChange={e => setTransportation(e.target.value)} value={transportation}/>
                     <div className="input-group-append">
                         <span className="input-group-text">Kms</span>
                     </div>
-                   <span class="ml-3">A pagar:</span>
+                   <span className="ml-3">A pagar:</span>
                 </div>
-                <div class="input-group col-sm-6 mb-3">
-                    <label for="meal" class="col-sm-4 col-form-label">Nº de comidas</label>
-                    <input type="number" class="form-control" name="meal"/> 
-                    <span class="ml-3">A pagar:</span>
+                <div className="input-group col-sm-6 mb-3">
+                    <label for="meal" className="col-sm-4 col-form-label">Nº de comidas</label>
+                    <input type="number" className="form-control" name="meal" onChange={e => setMeal(e.target.value)} value={meal}/> 
+                    <span className="ml-3">A pagar: {meal*2}</span>
                 </div>
-                <div class = "input-group col-sm-6 mb-3">
-                    <label for="parking" class="col-sm-4">Parking</label>       
-                    <input type="text" readonly class="form-control" name="parking" />
-                    <div class="input-group-append">
-                        <span class="input-group-text">$</span>
+                <div className="input-group col-sm-6 mb-3">
+                    <label for="parking" className="col-sm-4">Parking</label>       
+                    <input type="text"  className="form-control" name="parking" onChange={e => setParking(e.target.value)} value={parking}/>
+                    <div className="input-group-append">
+                        <span className="input-group-text">$</span>
                     </div>
-                    <span class="ml-3">A pagar:</span>
+                    <span className="ml-3">A pagar:</span>
                 </div>
              </div>
         </div>
