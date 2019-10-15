218156be3504dc4246fca0f6e491e6fa43f011e7
Patricia Defez <ptddds@gmail.com>
2019-10-15 20:51:23 +0200

    functions

--- allfunds/src/components/Form.js
+++ allfunds/src/components/Form.js
@@ -6,8 +6,46 @@ import React, { useState } from 'react';
 const Form = () => {
 
     const [transportation, setTransportation] = useState("");
-    const [meal, setMeal] = useState(0);
+    const [meal, setMeal] = useState("");
     const [parking, setParking] = useState("");
+
+    function payingTransp (n){
+        let toPayTransp = 0
+        if (!isNaN(n)){
+            if (n <= 100){
+                toPayTransp = n * 0.12
+            } else {
+                toPayTransp = ((n - 100) * 0.08) + 12
+            }
+        }
+        return toPayTransp.toFixed(2);
+    };
+
+    function payingMeal (n){
+        let toPayMeal = 0
+        if (!isNaN(n)){
+            n = Math.floor(n)
+            if (n <= 3){
+                toPayMeal = n * 10
+            } else {
+                toPayMeal = ((n - 3) * 6) + 30
+            }
+        }
+        return toPayMeal.toFixed(2);
+    };
+
+    function payingParking (n) {
+        let toPayParking = 0
+        if (!isNaN(n)){
+            n = Math.floor(n)
+            if (n <= 20){
+                toPayParking = n 
+            } else {
+                toPayParking = ((n - 20) / 2) + 20
+            }
+        }
+        return toPayParking.toFixed(2);
+    };
     
 
   
@@ -24,7 +62,7 @@ const Form = () => {
                     <div className="input-group-append">
                         <span className="input-group-text">Kms</span>
                     </div>
-                    <span className="ml-3">A pagar:</span>
+                    <span className="ml-3">A pagar: { payingTransp(transportation) } $</span>
                 </div>
                 {isNaN(transportation) && (
                     <p className="text-danger">Por favor, introduzca un número válido</p>
@@ -33,7 +71,7 @@ const Form = () => {
                 <div className="input-group col-sm-6 mb-3">
                     <label for="meal" className="col-sm-4 col-form-label">Nº de comidas</label>
                     <input type="text" className={`form-control ${isNaN(meal) ? 'text-danger is-invalid': ''}`} name="meal" onChange={e => setMeal(e.target.value)} value={meal} />
-                    <span className="ml-3">A pagar: {meal * 2}</span>
+                    <span className="ml-3">A pagar: { payingMeal(meal) } $</span>
                 </div>
                 {isNaN(meal) && (
                     <p className="text-danger">Por favor, introduzca un número válido</p>
@@ -45,7 +83,7 @@ const Form = () => {
                     <div className="input-group-append">
                         <span className="input-group-text">$</span>
                     </div>
-                    <span className="ml-3">A pagar:</span>
+                    <span className="ml-3">A pagar: { payingParking(parking) } $</span>
                 </div>
                 {isNaN(parking) && (
                     <p className="text-danger">Por favor, introduzca un número válido</p>
