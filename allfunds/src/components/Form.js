e4674c52f5141925d522b8974178f2d94317c1e2
Patricia Defez <ptddds@gmail.com>
2019-10-15 21:49:47 +0200

    get total

--- allfunds/src/components/Form.js
+++ allfunds/src/components/Form.js
@@ -10,59 +10,61 @@ const Form = () => {
     const [parking, setParking] = useState("");
 
     function payingTransp (n){
-        let toPayTransp = 0
+        let toPay = 0
         if (!isNaN(n)){
             if (n <= 100){
-                toPayTransp = n * 0.12
+                toPay = n * 0.12
             } else {
-                toPayTransp = ((n - 100) * 0.08) + 12
+                toPay = ((n - 100) * 0.08) + 12
             }
         }
-        return toPayTransp.toFixed(2);
+        return toPay.toFixed(2);
     };
 
     function payingMeal (n){
-        let toPayMeal = 0
+        let toPay = 0
         if (!isNaN(n)){
             n = Math.floor(n)
             if (n <= 3){
-                toPayMeal = n * 10
+                toPay = n * 10
             } else {
-                toPayMeal = ((n - 3) * 6) + 30
+                toPay = ((n - 3) * 6) + 30
             }
         }
-        return toPayMeal.toFixed(2);
+        return toPay.toFixed(2);
     };
 
     function payingParking (n) {
-        let toPayParking = 0
+        let toPay = 0
         if (!isNaN(n)){
             n = Math.floor(n)
             if (n <= 20){
-                toPayParking = n 
+                toPay = n 
             } else {
-                toPayParking = ((n - 20) / 2) + 20
+                toPay = ((n - 20) / 2) + 20
             }
         }
-        return toPayParking.toFixed(2);
+        return toPay.toFixed(2);
     };
     
-
+    const toPayTransp = payingTransp(transportation)
+    const toPayMeal = payingMeal(meal)
+    const toPayParking = payingParking(parking)
+    const toPayTotal = (toPayTransp * 1) + (toPayMeal * 1) + (toPayParking * 1)
   
 
     return (
+        
+
         <div className="row mx-auto ">
             <div className="col-xs-12 col-sm-10 mx-auto mt-5">
                 <div className="input-group  col-sm-6 mb-3">
                     <label for="transportation" className="col-sm-4">Desplazamientos</label>
-                    <input type="text" className={`form-control ${isNaN(transportation) ? 'text-danger is-invalid': ''}`} name="transportation"
-                        value={transportation}
-                        onChange={e => setTransportation(e.target.value)}
-                     />
+                    <input type="text" className={`form-control ${isNaN(transportation) ? 'text-danger is-invalid': ''}`} name="transportation" value={transportation} onChange={e => setTransportation(e.target.value)}/>
                     <div className="input-group-append">
                         <span className="input-group-text">Kms</span>
                     </div>
-                    <span className="ml-3">A pagar: { payingTransp(transportation) } $</span>
+                    <span className="ml-3">A pagar: { toPayTransp } $</span>
                 </div>
                 {isNaN(transportation) && (
                     <p className="text-danger">Por favor, introduzca un número válido</p>
@@ -71,7 +73,7 @@ const Form = () => {
                 <div className="input-group col-sm-6 mb-3">
                     <label for="meal" className="col-sm-4 col-form-label">Nº de comidas</label>
                     <input type="text" className={`form-control ${isNaN(meal) ? 'text-danger is-invalid': ''}`} name="meal" onChange={e => setMeal(e.target.value)} value={meal} />
-                    <span className="ml-3">A pagar: { payingMeal(meal) } $</span>
+                    <span className="ml-3">A pagar: { toPayMeal } $</span>
                 </div>
                 {isNaN(meal) && (
                     <p className="text-danger">Por favor, introduzca un número válido</p>
@@ -83,11 +85,13 @@ const Form = () => {
                     <div className="input-group-append">
                         <span className="input-group-text">$</span>
                     </div>
-                    <span className="ml-3">A pagar: { payingParking(parking) } $</span>
+                    <span className="ml-3">A pagar: { toPayParking } $</span>
                 </div>
                 {isNaN(parking) && (
                     <p className="text-danger">Por favor, introduzca un número válido</p>
                 )}
+                <div className="">Total a pagar: { toPayTotal } $</div>
+
             </div>
         </div>
     );
