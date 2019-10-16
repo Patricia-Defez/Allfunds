4c6b00daa9aa3161f83a96f02d8d553626e252a2
Patricia Defez <ptddds@gmail.com>
2019-10-16 19:37:01 +0200

    simplifying form

--- allfunds/src/components/Form.js
+++ allfunds/src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import FormField from './FormField';
 
 
 
@@ -20,57 +20,33 @@ const Form = () => {
             }
         }
         return toPay
-    }
-
-
-    
+    };    
     
     const toPayTransp = paying (transportation, 100, 0.12, 0.08)
     const toPayMeal = paying (meal, 3, 10, 6)
     const toPayParking = paying (parking, 20, 1, 0.5)
     const toPayTotal = (toPayTransp * 1) + (toPayMeal * 1) + (toPayParking * 1)
   
-
     return (
-        
-
         <div className="box mx-auto p-5">
         <div className="container  p-5">
             <div className=" mx-auto m-5 p-5 ">
-                <div className="input-group  mb-3">
-                    <label for="transportation" className="col-sm-2">Desplazamientos</label>
-                    <input type="text" className={`form-control ${isNaN(transportation) ? 'text-danger is-invalid': ''}`} name="transportation" value={transportation} onChange={e => setTransportation(e.target.value)}/>
-                    <div className="input-group-append">
-                        <span className="input-group-text">Kms</span>
-                    </div>
-                    <span className="col-sm-2 ml-3 text-right">A pagar: { toPayTransp } $</span>
+                <div className="row">
+                    <FormField title="Desplazamientos" name="transportation" role="Kms" vari={toPayTransp}
+                        value={transportation}
+                        onChange={e => setTransportation(e.target.value)}/>                   
+                </div> 
+                <div className="row">
+                    <FormField title="Nº de comidas" name="meal" role="" vari={toPayMeal}
+                        value={meal}
+                        onChange={e => setMeal(e.target.value)}/>
+                </div> 
+                <div className="row">
+                    <FormField title="Parking" name="parking" role="$" vari={toPayParking}
+                        value={parking}
+                        onChange={e => setParking(e.target.value)}/>
                 </div>
-                {isNaN(transportation) && (
-                    <p className="text-danger">Por favor, introduzca un número válido</p>
-                )}
-                
-                <div className="input-group  mb-3">
-                    <label for="meal" className="col-sm-2 col-form-label">Nº de comidas</label>
-                    <input type="text" className={`form-control ${isNaN(meal) ? 'text-danger is-invalid': ''}`} name="meal" onChange={e => setMeal(e.target.value)} value={meal} />
-                    <span className="col-sm-2 ml-3 text-right">A pagar: { toPayMeal } $</span>
-                </div>
-                {isNaN(meal) && (
-                    <p className="text-danger">Por favor, introduzca un número válido</p>
-                )}
-
-                <div className="input-group  mb-3">
-                    <label for="parking" className="col-sm-2">Parking</label>
-                    <input type="text" className={`form-control ${isNaN(parking) ? 'text-danger is-invalid': ''}`} name="parking" onChange={e => setParking(e.target.value)} value={parking} />
-                    <div className="input-group-append">
-                        <span className="input-group-text">$</span>
-                    </div>
-                    <span className="col-sm-2 ml-3 text-right">A pagar: { toPayParking } $</span>
-                </div>
-                {isNaN(parking) && (
-                    <p className="text-danger">Por favor, introduzca un número válido</p>
-                )}
-                <div className="text-right font-weight-bold">Total a pagar: { toPayTotal } $</div>
-
+                <div className="text-right font-weight-bold">Total a pagar: { toPayTotal } $</div>    
             </div>
             </div>
         </div>
---
+++ allfunds/src/components/FormField.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default (props) => (
+    <div className="input-group  mb-3">
+        <label className="col-sm-2">{props.title}</label>
+        <input 
+            type={props.type || "text"}
+            className={`form-control ${isNaN(props.value) ? 'text-danger is-invalid': ''}`} 
+            name={props.name} 
+            value={props.value} 
+            onChange={props.onChange}/>
+        <div className="input-group-append">
+            <span className="input-group-text">{props.role}</span>
+        </div>
+        <span className="col-sm-2 ml-3 text-right">A pagar: { props.vari } $</span>
+        {isNaN(props.value) && (
+            <p className="text-danger">Por favor, introduzca un número válido</p>
+        )}
+    </div>
+);
